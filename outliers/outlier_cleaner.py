469922a3996cdbf6603d07baf43817fac807a32e
liuyepiaoxiang <liujia19891217@126.com>
2017-07-20 07:55:56 +0800

    尝试删除10%的错误

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,17 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    maxP = []
+    for i in range(len(predictions)):
+        maxP[i] = predictions[i][0]
+        for j in range(len(predictions) / 10):
+            maxindex = maxP.index(max(maxP))
+            maxP.remove(max(maxP))
+            cleaned_data[i] = (ages[maxindex], net_worths(maxindex), predictions[maxindex])
 
-    
     return cleaned_data
 
